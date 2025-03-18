3ec88bcd0f19ebd5eb94adb08db60b3f315f88c0
Josef Ameur <148804322+ambrosia-fish@users.noreply.github.com>
2025-03-18 12:53:37 -0400

    Add authentication middleware to instance routes

--- src/routes/instances.js
+++ src/routes/instances.js
@@ -6,8 +6,12 @@ const {
   updateInstance,
   deleteInstance
 } = require('../controllers/instanceController');
+const { authenticate } = require('../middleware/auth');
 
 const router = express.Router();
+
+// Apply authentication middleware to all instance routes
+router.use(authenticate);
 
 router.route('/')
   .get(getInstances)
