a80421ea561dd97be2f5189c89b06bdc7096f5cf
ambrosia-fish <148804322+ambrosia-fish@users.noreply.github.com>
2025-03-25 09:16:37 -0400

    Delete deprecated files

--- src/models/BlacklistedToken.js
+++
@@ -1,8 +0,0 @@
-// This file has been removed as part of the security simplification
-// It is kept as an empty file for backward compatibility
-const mongoose = require('mongoose');
-
-// Empty schema
-const BlacklistedTokenSchema = new mongoose.Schema({});
-
-module.exports = mongoose.model('BlacklistedToken', BlacklistedTokenSchema);
